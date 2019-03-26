86f74a4f4e7fbf50014b2c5411c42fa71a94a332
Amjed Agabani <amjed.agabani@centtrip.com>
2019-03-26 16:21:55 +0000

    property recap

--- design-patterns/src/task.js
+++ design-patterns/src/task.js
@@ -2,7 +2,16 @@ var task = {
     title: 'My Title',
     description: 'My Description'
 };
-task.toString = function () {
-    return this.title + ' ' + this.description;
-}
-console.log(task.toString());
+
+Object.defineProperty(task, 'toString', {
+    value: function () {
+        return this.title + ' ' + this.description;
+    },
+    writable: false,
+    enumerable: false,
+    configurable: true
+});
+
+task.toString = "hi";
+
+console.log(Object.keys(task));
